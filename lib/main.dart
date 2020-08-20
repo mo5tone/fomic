06379f15c965b32d8e419d15b5c64c744eae79e7
mo5tone <8422976+mo5tone@users.noreply.github.com>
2020-08-20 15:24:07 +0800

    Set text font to google font: Ma Shan Zheng

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:fomic/scene/books/viewmodel/books_view_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import 'model/constant/repository_id.dart';
@@ -21,6 +22,9 @@ class Fomic extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.maShanZhengTextTheme(Theme.of(context).textTheme),
+        primaryTextTheme: GoogleFonts.maShanZhengTextTheme(Theme.of(context).primaryTextTheme),
+        accentTextTheme: GoogleFonts.maShanZhengTextTheme(Theme.of(context).accentTextTheme),
       ),
       builder: (ctx, child) => Material(
         type: MaterialType.transparency,
