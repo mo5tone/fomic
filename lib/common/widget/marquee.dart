0a45f9680a125d86c7e7d442ed09e1820effdc3d
mo5tone <8422976+mo5tone@users.noreply.github.com>
2022-04-27 14:49:22 +0800

    Keep Marquee's speed at same when child size change

--- lib/common/widget/marquee.dart
+++ lib/common/widget/marquee.dart
@@ -20,19 +20,20 @@ class Marquee extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final scrollController = useScrollController();
     useEffect(() {
+      const stayDuration = Duration(milliseconds: 500);
       var count = 0;
-      final timer = Timer.periodic(Duration(milliseconds: duration.inMilliseconds + 500), (timer) async {
-        final offset = count % 2 == 1 ? scrollController.position.minScrollExtent : scrollController.position.maxScrollExtent;
-        await scrollController.animateTo(
-          offset,
-          duration: duration,
-          curve: Curves.linear,
-        );
-        count += 1;
+      Timer? timer;
+      scrollController.addListener(() {
+        if (timer == null && scrollController.hasClients) {
+          final animationDuration = duration * (scrollController.position.extentInside / 200);
+          timer = Timer.periodic(animationDuration + stayDuration, (timer) {
+            final offset = count % 2 == 1 ? scrollController.position.minScrollExtent : scrollController.position.maxScrollExtent;
+            scrollController.animateTo(offset, duration: animationDuration, curve: Curves.linear).then((_) => count += 1);
+          });
+        }
       });
-      return () {
-        timer.cancel();
-      };
+      Future.delayed(stayDuration).then((_) => scrollController.jumpTo(1)); // Trigger scrollController's listeners
+      return () => timer?.cancel();
     }, [scrollController]);
     return SingleChildScrollView(
       child: child,
